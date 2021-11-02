db55b23359f591beafc30c2eb6e10d8819a9b612
Kittitat Bamrung (Student) <N00201327@iadt.ie>
2021-11-02 15:59:36 +0000

    first ep

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <article>
+        <?= $post; =?>
+    </article>
+</body>
+</html>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My First Post</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores consequatur expedita odio cum harum, eum rerum quis similique sapiente earum eligendi assumenda veritatis provident? Similique in libero fugiat alias, non atque ab!</p>
+</body>
+</html> -->
